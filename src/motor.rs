2f64ecdde7de5149e1f5c0e07bc50a2224fccd95
Leonti <prishelec@gmail.com>
2018-11-12 22:40:13 +1100

    Allow to start on local machine

--- src/motor.rs
+++ src/motor.rs
@@ -9,15 +9,15 @@ pub struct Motor {
 
 impl Motor {
 
-    pub fn new() -> Motor {
-        let i2cdevice = LinuxI2CDevice::new("/dev/i2c-1", DEFAULT_PCA9685_ADDRESS).unwrap();
+    pub fn new() -> Result<Motor, LinuxI2CError> {
+        let i2cdevice = LinuxI2CDevice::new("/dev/i2c-1", DEFAULT_PCA9685_ADDRESS)?;
 
-        let mut pca = PCA9685::new(i2cdevice).unwrap();
-        pca.set_pwm_freq(60.0).unwrap();
+        let mut pca = PCA9685::new(i2cdevice)?;
+        pca.set_pwm_freq(60.0)?;
 
-        Motor {
+        Ok(Motor {
             pca
-        }
+        })
     }
 
     pub fn set_speed(&mut self) -> () {
--- src/motor_handler.rs
+++ src/motor_handler.rs
@@ -12,7 +12,7 @@ type Tx = mpsc::UnboundedSender<MotorCommand>;
 
 pub struct MotorHandler {
     rx: Rx,
-    motor: Arc<Mutex<Motor>>
+    motor: Arc<Mutex<Option<Motor>>>
 }
 
 impl MotorHandler {
@@ -20,16 +20,24 @@ impl MotorHandler {
     pub fn new() -> (MotorHandler, Tx) {
         let (tx, rx) = mpsc::unbounded();
 
-        let motor = Motor::new();
-        (MotorHandler { rx, motor: Arc::new(Mutex::new(motor)) }, tx)
+        match Motor::new() {
+            Ok(motor) => (MotorHandler { rx, motor: Arc::new(Mutex::new(Some(motor))) }, tx),
+            Err(e) => {
+                println!("Error creating a motor {:?}", e);
+                (MotorHandler { rx, motor: Arc::new(Mutex::new(None)) }, tx)
+            }
+        }
+
     }
 
     pub fn run(self) -> impl Future<Item = (), Error = ()> {
         let motor_arc = self.motor;
         self.rx.for_each(move |_| {
-            println!("Received command");
-            let mut motor = motor_arc.lock().unwrap();
-            motor.set_speed();
+            println!("Received motor command");
+            let mut motor_option = motor_arc.lock().unwrap();
+
+            motor_option.as_mut().map(|motor| motor.set_speed());
+
             Ok(())
         }).map_err(|err| {
             println!("command reading error = {:?}", err);
